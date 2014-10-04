d1f754b478950678a0f9255ea0215b7042e6c9ed
Gabriel Vieira <viera_frifri@hotmail.com>
2014-10-03 21:00:09 -0300

    crawler inicial comentado

--- crawler/craw.py
+++ crawler/craw.py
@@ -13,24 +13,28 @@ def crawlar(browser , numPagina):
 browser = webdriver.Firefox()
 browser.get('http://ciagri.iea.sp.gov.br/nia1/precos_medios.aspx?cod_sis=4')
 
+#CHECAR TODOS OS PRODITOS
 element = browser.find_element_by_id('chkTodos')
 element.click()
 
+#CLICAR NO BOTAO DE PESQUISAR
 element = browser.find_element_by_id('imgPesquisar')
 element.click()
 
-#wait = WebDriverWait(browser, 10)
+#ESPERAR CARREGAR
 browser.implicitly_wait(10) 
 
 
 #CONTAR AS PEGINAS GERADAS
 count = 0
 
+#PROCURA NA TABELA A ULTIMA LINHA QUE CONTEM A QUANTIDADE DE PAGINAS
 linhas = browser.find_element_by_id('Grid').find_elements_by_tag_name('tr')
 
 for linha in linhas:
 	ultimo = linha;
 
+#CONTA QUANTAS PAGINAS FORAM GERADAS
 paginas = ultimo.find_element_by_tag_name('td').find_elements_by_tag_name('a')
 
 for pagina in paginas:
@@ -41,16 +45,18 @@ for pagina in paginas:
 			count = int(pagina.text)
 			
 
+#PARA A QUANTIDADE DE PAGINAS QUE FORAM GERADAS
 for x in range(1, count + 1):
 	
+	#PEGA AS INFORMACOES DAS LINHAS
 	linhas = browser.find_element_by_id('Grid').find_elements_by_tag_name('tr')
 
 	for linha in linhas:
 		ultimo = linha;
 		print(linha.text)
 
+	#IR PARA A PROXIMA PAGINA
 	paginas = ultimo.find_element_by_tag_name('td').find_elements_by_tag_name('a')
-
 	for pagina in paginas:
 		if int(pagina.text) == x+1:
 			pagina.click()

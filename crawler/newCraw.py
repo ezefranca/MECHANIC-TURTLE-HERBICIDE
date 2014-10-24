8c8066e98bcab82318304c2fbd0dd86bce101f9d
Gabriel Vieira <viera_frifri@hotmail.com>
2014-10-24 21:40:24 -0200

    CRAWLER FINAL XABLAU

--- crawler/newCraw.py
+++ crawler/newCraw.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import psycopg2
 from contextlib import closing
 from selenium.webdriver import Firefox # pip install selenium
 from selenium import webdriver
@@ -7,8 +8,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 #EXEMPLO MANEIRO DE FUNCAO
 def crawlar(browser , numPagina):
 	return
-
-# use firefox to get page with javascript generated content
+#INICIAR A CONECCAO COM O BANCO
+conn = psycopg2.connect("host=23.89.198.31 dbname=magikarp user=gabriel password=8963")
+cur = conn.cursor()
 
 #INICIAR
 browser = webdriver.Firefox()
@@ -17,6 +19,7 @@ browser.get('http://ciagri.iea.sp.gov.br/nia1/precos_medios.aspx?cod_sis=4')
 #CHECAR TODOS OS PRODITOS
 element = browser.find_element_by_id('chkTodos')
 element.click()
+
 
 #SETAR DATAS
 browser.find_element_by_xpath("//select[@id='cmbPeriodo_Inicial']/option[@value='1970/MAI']").click()
@@ -54,16 +57,35 @@ for pagina in paginas:
 
 #MARCAR O NUMERO DA PAGINA
 numPagina = 1
-
+first = 1
+focaGay = 20
 #PARA CADA GRUPO DE PAGINAS
-for x in xrange(1,3):
+for x in xrange(1,30):
 	#PARA CADA PAGINA
-	for x in range(1, 20):
+	for x in range(1, 21):
 		#PEGA AS INFORMACOES DAS LINHAS
 		linhas = browser.find_element_by_id('Grid').find_elements_by_tag_name('tr')
+		#PARA CADA LINHA
 		for linha in linhas:
-			ultimo = linha;
-			print(linha.text)
+			#NAO PEGAR A PRIMEIRA LINHA PQ E A DESCRICAO DAS PARADAS
+			if not (first):
+				ultimo = linha;
+				#CRIAR O OBEJTO MANEIRO DA COLUNA
+				coluna = linha.find_elements_by_tag_name('td')
+				#VER SE E UMA COLUNA VALIDA
+				if (len(coluna) == 6):
+					num = coluna[4].text
+					num = num.replace("." , "")
+					num = num.replace("," , ".")
+					#GERAR A QUERY PARA INSERIR
+					query = "INSERT INTO preco_mensal_varejo( produto, mes, ano, moeda, preco, unidade) \
+					VALUES ( '%s', '%s', '%s', '%s' ,  '%f' , '%s')" % (coluna[0].text , coluna[1].text , coluna[2].text , coluna[3].text ,float(num) , coluna[5].text)
+					#INSERIR
+					#cur.execute(query)
+					#conn.commit()
+					print(query)
+			else:
+				first = 0	
 
 		#IR PARA A PROXIMA PAGINA
 		paginas = ultimo.find_element_by_tag_name('td').find_elements_by_tag_name('a')
@@ -72,6 +94,7 @@ for x in xrange(1,3):
 				if int(pagina.text) == numPagina+1:
 					numPagina = numPagina + 1
 					pagina.click()
+					first = 1
 					browser.implicitly_wait(10)
 					break 
 		print(numPagina)
@@ -84,10 +107,17 @@ for x in xrange(1,3):
 	#PASSAR PARA O PROXIMO GRUPO DE PAGINAS GG
 	paginas = ultimo.find_element_by_tag_name('td').find_elements_by_tag_name('a')
 
+	buceta = 0
 	for pagina in paginas:
-		if pagina.text == "...":
+		buceta = buceta + 1
+		if (pagina.text == "...") & (buceta > 4):
 			numPagina = numPagina + 1
 			pagina.click()
+			first = 1
+			focaGay = 22
 
 
+
+cur.close()
+conn.close()
 browser.close()
